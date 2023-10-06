12114f90eb05a62270ed208a1a4e23e0916448b7
Mariano3860 <72263339+Mariano3860@users.noreply.github.com>
2023-10-06 15:09:20 +0200

    More details in plan implementation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ai_helper import create_list_with_call_ai, create_object_with_call_ai
 
 if __name__ == '__main__':
     characters = []
-    story = "Game of thrones"
+    story = "Harry potter"
     max_items = 30
     item_type = "names"
     list_names = create_list_with_call_ai(item_type, max_items, story, 1)
@@ -43,17 +43,30 @@ if __name__ == '__main__':
                     if actions:
                         print(f"{character.get_name()}: {', '.join(actions)}")
 
-# OnError: repeat 3 times, if not, change promp, if not, message error
-# Ask for a list of names, filter uniques, ask for more names if needed given the already generated list.
-# Using list of names, ask for a list of x actions per name
-# Using list of names and/or actions, ask for a list of x appearance modifiers
-# Ask for a background description for all those characters, store it in a class, well organized.
-# Give maximum width-tall (maxXY) and ask for XY position for each character
-# Generate an image per character using each class and store it in each character class.image
-# Generate a background with the background class descriptions and the maxXY
-# Paste each character on top of the image using XY position.
-# Optional: Create also classes of objects and buildings, generate images and paste them in background.
-# Generate a new polished image with the sketched image.
-# Ask an IA for validation of the image created, use info inside classes to check presence, position, quality.
-# Generate percentages of compliance/efficiency, in the future use them to feedback the AIs.
-# Store information in logs in order to rebuild process.
+#   Ask for a list of names, filter uniques, ask for more names if needed given the already generated list.
+#       OnError: repeat 3 times, if not, change prompt, if not, message error
+#   Using list of names, ask for a list of x actions per name
+#       OnError: repeat 3 times, if not, change prompt, if not, message error
+#   Using list of names and/or actions, ask for a list of x appearance modifiers
+#       OnError: repeat 3 times, if not, change prompt, if not, message error
+#   Ask for a background description for all those characters, store it in a class, well organized.
+#       OnError: repeat 3 times, if not, change prompt, if not, message error
+#   Give maximum width-tall (maxXY) and create XY position for each character
+#       Create a map(x,y) with maxXY, and randomize position of character
+#           Mark on map occupied surface of each character, randomize over free spaces.
+#           Position of each character can start in top left corner and consider surface with width and length.
+#   Generate an image per character using each class and store it in each character class.image
+#       Generate a string Character.getDescription() method
+#       Generate the image, create a right prompt and right parameters needed
+#       Use or create a test to check if character has face, full-body, if not, repeat process.
+#   Generate a background with the background class descriptions and the maxXY
+#       Validate background somehow, if not repeat process
+#   Paste each character on top of the image using XY position.
+#   Optional: Create also classes of objects and buildings, generate images and paste them in background.
+#   Generate a new polished image with the sketched image.
+#       Can ask in that generation for interaction between objects.
+#       Ask an IA for validation of the image created, use info inside classes to check presence, position, quality.
+#       If validation is not passed, then regenerate image.
+#       Generate percentages of compliance/efficiency, in the future use them to feedback the AIs.
+#   Store information in logs in order to rebuild process.
+#       Store all the used seeds, in text and image.
